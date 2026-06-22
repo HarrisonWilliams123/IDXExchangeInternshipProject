83cba5700e5cb3f17323c46ce75ca2eef305fe54
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-06-22 00:44:23 -0400

    Finished Deliverable - Week 1

--- src/listing_data_script/listing_data_aggregation.py
+++ src/listing_data_script/listing_data_aggregation.py
@@ -8,13 +8,34 @@ all_listing_files = glob.glob(listing_file_pattern)
 #Initialize an empty list to store the individual data frames
 listing_df_list = []
 
+#Initialize an empty list to find the average of entries before
+total_entries = []
+
 #Use a for loop to read each file and add it to the list
 for filename in all_listing_files:
     listing_df = pd.read_csv(filename)
+    #Adds the amount of rows before
+    total_entries.append(len(listing_df))
     listing_df_list.append(listing_df)
 
 #Concatenates all data frames to create final listing data frame
 combined_listing_df = pd.concat(listing_df_list, ignore_index=True)
 
+#Prints the amount of rows after
+print(round(sum(total_entries) / len(total_entries), 0))
+print(len(combined_listing_df))
+
+#Before total amount of rows before concatenation: 31914 rows
+#After total amount of rows after concatenation: 893594 rows
+
+#Filters the Property Type to 'Residential' only
+combined_listing_df = combined_listing_df[combined_listing_df['PropertyType'] == 'Residential']
+
+#Prints the amount of rows after the filter
+print(len(combined_listing_df))
+
+#Before total amount of rows before filter: 893594 rows
+#After total amount of rows after filter: 567549 rows
+
 #Save the combined data frame to a new .csv file
-combined_listing_df.to_csv("combined_listing_df.csv", index=False)
+combined_listing_df.to_csv("combined_datasets/combined_listing_df.csv", index=False)
--- src/sold_data_script/sold_data_aggregation.py
+++ src/sold_data_script/sold_data_aggregation.py
@@ -8,14 +8,35 @@ all_sold_files = glob.glob(sold_file_pattern)
 #Initialize an empty list to store the individual data frames
 sold_df_list = []
 
+#Initialize an empty list to find the average of entries before
+total_entries = []
+
 #Use a for loop to read each file and add it to the list
 for filename in all_sold_files:
     sold_df = pd.read_csv(filename)
+    #Adds the amount of rows before
+    total_entries.append(len(sold_df))
     sold_df_list.append(sold_df)
 
 #Concatenates all data frames to create final sold data frame
 combined_sold_df = pd.concat(sold_df_list, ignore_index=True)
 
+#Prints the amount of rows after
+print(round(sum(total_entries) / len(total_entries), 0))
+print(len(combined_sold_df))
+
+#Before total amount of rows before concatenation: 21987 rows
+#After total amount of rows after concatenation: 681599 rows
+
+#Filters the Property Type to 'Residential' only
+combined_sold_df = combined_sold_df[combined_sold_df['PropertyType'] == 'Residential']
+
+#Prints the amount of rows after the filter
+print(len(combined_sold_df))
+
+#Before total amount of rows before filter: 681599 rows
+#After total amount of rows after filter: 458336 rows
+
 #Save the combined data frame to a new .csv file
-combined_sold_df.to_csv("combined_sold_df.csv", index=False)
+combined_sold_df.to_csv("combined_datasets/combined_sold_df.csv", index=False)
 
