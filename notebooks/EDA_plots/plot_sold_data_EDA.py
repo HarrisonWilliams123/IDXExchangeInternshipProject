44fc760bdad2ad92a51d67566dd46da83b8dcf3b
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-06-24 12:30:07 -0400

    Created bar graphs for the sold dataset, and changed the name of the listing bar graphs

--- notebooks/EDA_plots/plot_sold_data_EDA.py
+++ notebooks/EDA_plots/plot_sold_data_EDA.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #Opens the combined dataset from the first deliverable
-listing_data = pd.read_csv("data/unfiltered_listing_df.csv")
+sold_data = pd.read_csv("data/unfiltered_sold_df.csv")
 
 #Prints out the different types of PropertyType
-print(listing_data['PropertyType'].unique())
+print(sold_data['PropertyType'].unique())
 
 #Gets the specific amount of times a PropertyType comes up (e.g. Residential, Land, ComercialLease, etc.)
-number_of_residential_properties = len(listing_data[listing_data['PropertyType'] == 'Residential'])
-number_of_commercialsale_properties = len(listing_data[listing_data['PropertyType'] == 'CommercialSale'])
-number_of_manufacturedinpark_properties = len(listing_data[listing_data['PropertyType'] == 'ManufacturedInPark'])
-number_of_residentiallease_properties = len(listing_data[listing_data['PropertyType'] == 'ResidentialLease'])
-number_of_land_properties = len(listing_data[listing_data['PropertyType'] == 'Land'])
-number_of_residentialincome_properties = len(listing_data[listing_data['PropertyType'] == 'ResidentialIncome'])
-number_of_commerciallease_properties = len(listing_data[listing_data['PropertyType'] == 'CommercialLease'])
-number_of_businessopportunity_properties = len(listing_data[listing_data['PropertyType'] == 'BusinessOpportunity'])
+number_of_residential_properties = len(sold_data[sold_data['PropertyType'] == 'Residential'])
+number_of_commercialsale_properties = len(sold_data[sold_data['PropertyType'] == 'CommercialSale'])
+number_of_manufacturedinpark_properties = len(sold_data[sold_data['PropertyType'] == 'ManufacturedInPark'])
+number_of_residentiallease_properties = len(sold_data[sold_data['PropertyType'] == 'ResidentialLease'])
+number_of_land_properties = len(sold_data[sold_data['PropertyType'] == 'Land'])
+number_of_residentialincome_properties = len(sold_data[sold_data['PropertyType'] == 'ResidentialIncome'])
+number_of_commerciallease_properties = len(sold_data[sold_data['PropertyType'] == 'CommercialLease'])
+number_of_businessopportunity_properties = len(sold_data[sold_data['PropertyType'] == 'BusinessOpportunity'])
 sum_of_other_property_types = number_of_businessopportunity_properties + number_of_commercialsale_properties + number_of_land_properties + number_of_commerciallease_properties + number_of_manufacturedinpark_properties + number_of_residentialincome_properties + number_of_residentiallease_properties
 
 
@@ -50,7 +50,7 @@ plt.xticks(rotation=45)
 plt.bar_label(bars, padding=3, fontweight='bold')
 
 plt.tight_layout()
-plt.savefig("notebooks/graphs/property_types_comparison.png", dpi=300)
+plt.savefig("notebooks/graphs/sold_property_types_comparison.png", dpi=300)
 
 
 #Second Bar Chart
@@ -67,6 +67,4 @@ plt.ylabel('Quantity')
 plt.bar_label(bars2, padding=3, fontweight='bold')
 
 plt.tight_layout()
-plt.savefig("notebooks/graphs/residential_vs_other.png", dpi=300)
-
-
+plt.savefig("notebooks/graphs/sold_residential_vs_other.png", dpi=300)
